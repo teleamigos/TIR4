06e09ad5b46723426c514d4af4b12e0a89607a66
Jorge Orlando González Guzmán <43627662+OrlandoTele96@users.noreply.github.com>
2019-10-17 16:05:33 -0500

    Add files via upload

--- MPR.py
+++ MPR.py
@@ -1,22 +1,24 @@
 """-----------------------------------------------------------------------------
 ---------------------------------MPR-----------------------------------------"""
 
-file=open('Nodes.txt','r')
-d=dict()
-lines=file.readlines()
-for line in lines:
-    line=line.split('\n')[0]
-    Nodes=line.split(';')[0]
-    line=line[2:]
-    Neighbor=line.split(',')
-    d[Nodes]=Neighbor
-n_ord=[]
-for n in d['2']:
-    N_v=len(d[n])
-    n_ord.append((n,N_v))
+import csv
+Neighbors=[]
+tp=dict()
+with open('Nodes.csv', newline='') as File:
+    reader = csv.reader(File)
+    for row in reader:
+        Node=row[0]
+        row=row[1:]
+        #Neighbors.clear()
+        for column in row:
+            if column!='':
+                Neighbors.append(column)
+        tp[Node]=Neighbors
+        Neighbors=[]
+    File.close()
 aux=0
-n_ord_aux=[]
-for n in n_ord:
-    if n[1]>aux:
-        n_ord_aux.append(n)
-        aux=n[1]
+Nodes_ord=[]
+for Node in tp:
+    L=len(tp[Node])
+    if L>=aux:
+        
